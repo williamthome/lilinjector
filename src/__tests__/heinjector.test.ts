734c64305d545e6405b9b0180e798e1822e4b2c0
William Fank Thomé <willegp88@gmail.com>
2020-11-11 00:13:12 -0300

    test: ensure push to array

--- src/__tests__/heinjector.test.ts
+++ src/__tests__/heinjector.test.ts
@@ -20,47 +20,77 @@ interface Payload<P> {
 
 type PayloadReturnType = 'newable' | 'factory' | 'value' | 'array'
 
-class SingletonConfig<P> {
+class SaveConfig<I, P> {
   constructor (
-    private readonly payload: Payload<P>
+    private readonly container: Container,
+    private readonly identifier: Identifier<I>,
+    private readonly payload: Payload<P>,
+    private readonly overrideRegistry: boolean
+  ) { }
+
+  save = (): void => {
+    if (this.overrideRegistry)
+      this.container.override(this.identifier, this.payload)
+  }
+}
+
+class SingletonConfig<I, P> {
+  constructor (
+    private readonly container: Container,
+    private readonly identifier: Identifier<I>,
+    private readonly payload: Payload<P>,
+    private readonly overrideRegistry: boolean
   ) { }
 
   notInSingletonScope = (): void => {
     this.payload.singleton = false
+    new SaveConfig(this.container, this.identifier, this.payload, this.overrideRegistry)
   }
 }
 
-class ArrayConfig<P> {
+class ArrayConfig<I, P> {
   constructor (
+    private readonly container: Container,
+    private readonly identifier: Identifier<I>,
     private readonly payload: Payload<P>,
-    private readonly newValues: P[]
+    private readonly newValues: P[],
+    private readonly overrideRegistry: boolean
   ) { }
 
   override = (): void => {
     this.payload.array = this.newValues
+    new SaveConfig(this.container, this.identifier, this.payload, this.overrideRegistry)
   }
 }
 
-class Bind<P> {
-  constructor (private payload: Payload<P>) { }
+class Bind<I, P> {
+  constructor (
+    private readonly container: Container,
+    private readonly identifier: Identifier<I>,
+    private payload: Payload<P>,
+    private readonly overrideRegistry = false
+  ) { }
 
-  asNewable = (newable: Newable<P>): SingletonConfig<P> => {
+  asNewable = (newable: Newable<P>): SingletonConfig<I, P> => {
     this.payload.newable = newable
-    return new SingletonConfig<P>(this.payload)
+    new SaveConfig(this.container, this.identifier, this.payload, this.overrideRegistry)
+    return new SingletonConfig<I, P>(this.container, this.identifier, this.payload, this.overrideRegistry)
   }
 
-  asFactory = (factory: Factory<P>): SingletonConfig<P> => {
+  asFactory = (factory: Factory<P>): SingletonConfig<I, P> => {
     this.payload.factory = factory
-    return new SingletonConfig<P>(this.payload)
+    new SaveConfig(this.container, this.identifier, this.payload, this.overrideRegistry)
+    return new SingletonConfig<I, P>(this.container, this.identifier, this.payload, this.overrideRegistry)
   }
 
   as = (value: P): void => {
     this.payload.value = value
   }
 
-  asArray = (...array: P[]): ArrayConfig<P> => {
-    this.payload.array = [...array, ...this.payload.array || []]
-    return new ArrayConfig<P>(this.payload, array)
+  asArray = (...array: P[]): ArrayConfig<I, P> => {
+    this.payload.array = [...this.payload.array || [], ...array]
+    new SaveConfig(this.container, this.identifier, this.payload, this.overrideRegistry)
+    return new ArrayConfig<I, P>(this.container, this.identifier, this.payload, array, this.overrideRegistry)
   }
 }
 
@@ -69,8 +99,8 @@ type Registry<I, P> = Map<Identifier<I>, Payload<P>>
 class Container {
   private readonly _registry: Registry<any, any> = new Map()
 
-  bind = <I, P> (identifier: Identifier<I>): Bind<P> => {
-    return new Bind(this._add<I, P>(identifier))
+  bind = <I, P> (identifier: Identifier<I>): Bind<I, P> => {
+    return new Bind<I, P>(this, identifier, this._add<I, P>(identifier))
   }
 
   unbind = <P> (identifier: Identifier<P>): Container => {
@@ -82,17 +112,24 @@ class Container {
     return this
   }
 
-  rebind = <I, P> (identifier: Identifier<I>): Bind<P> => {
+  rebind = <I, P> (identifier: Identifier<I>): Bind<I, P> => {
     return this.unbind<I>(identifier).bind<I, P>(identifier)
   }
 
-  define = <I, P> (identifier: Identifier<I>): Bind<P> => {
+  define = <I, P> (identifier: Identifier<I>): Bind<I, P> => {
     const registered = this._registry.get(identifier)
 
     if (!registered)
       throw new Error(`Identifier ${identifier.toString()} not in registry`)
 
-    return new Bind(registered)
+    return new Bind<I, P>(this, identifier, registered, true)
+  }
+
+  override = <I, P> (identifier: Identifier<I>, payload: Payload<P>): void => {
+    if (!this._registry.has(identifier))
+      throw new Error(`Identifier ${identifier.toString()} not in registry`)
+
+    this._registry.set(identifier, payload)
   }
 
   get = <P> (identifier: Identifier<P>, payloadReturnType?: PayloadReturnType): P | P[] => {
@@ -196,6 +233,18 @@ describe('Heinjector', () => {
       container.define('foo').as('bar')
       foo = container.get('foo')
       expect(foo).toBe('bar')
+    })
+
+    it('should push to array', () => {
+      const container = new Container()
+
+      container.bind('foo').asArray('foo')
+      let foo = container.get('foo')
+      expect(foo).toEqual(['foo'])
+
+      container.define('foo').asArray('bar')
+      foo = container.get('foo')
+      expect(foo).toEqual(['foo', 'bar'])
     })
   })
 })
