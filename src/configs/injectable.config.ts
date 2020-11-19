0f5a442acb00ac7f513408c5329d2e5bf98e7a4a
William Fank Thomé <willegp88@gmail.com>
2020-11-19 11:41:54 -0300

    feat: create injectable config

---
+++ src/configs/injectable.config.ts
@@ -0,0 +1,33 @@
+import { Container } from 'src/container'
+import { Identifier, Newable } from 'src/protocols'
+
+export class InjectableConfig<TIdentifier extends Record<string, any>, TTarget extends Newable<any>> {
+
+  constructor (
+    private readonly target: TTarget,
+    private readonly container: Container,
+    private readonly identifier?: Identifier<TIdentifier>
+  ) {}
+
+  asNewable = (): TTarget => {
+    if (this.container.has(this._identifier)) {
+      this.container.define<TIdentifier, TTarget>(this._identifier).asNewable(this.target).done()
+    } else {
+      this.container.bind(this._identifier).asNewable(this.target).done()
+    }
+    return this.target
+  }
+
+  asNewableArray = (): TTarget => {
+    if (this.container.has(this._identifier)) {
+      this.container.define<TIdentifier, TTarget>(this._identifier).asNewableArray(this.target).done()
+    } else {
+      this.container.bind(this._identifier).asNewableArray(this.target).done()
+    }
+    return this.target
+  }
+
+  private get _identifier(): Identifier<TIdentifier> | TTarget {
+    return this.identifier || this.target
+  }
+}
