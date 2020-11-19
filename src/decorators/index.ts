80c392bed87b477ad1fbc2ef15c40b12a5a81d46
William Fank Thomé <willegp88@gmail.com>
2020-11-19 11:43:13 -0300

    feat: create injectable array decorator

--- src/decorators/index.ts
+++ src/decorators/index.ts
@@ -1,2 +1,3 @@
 export * from './inject.decorator'
 export * from './injectable.decorator'
+export * from './injectable-array.decorator'
---
+++ src/decorators/injectable-array.decorator.ts
@@ -0,0 +1,11 @@
+import { InjectableConfig } from '../configs'
+import { Container } from '../container'
+import { Identifier, Newable } from '../protocols'
+
+export const InjectableArray = <TIdentifier extends Record<string, any>> (container: Container, identifier?: Identifier<TIdentifier>) => {
+  return <TTarget extends Newable<any>> (
+    target: TTarget
+  ): TTarget => {
+    return new InjectableConfig<TIdentifier, TTarget>(target, container, identifier).asNewableArray()
+  }
+}
