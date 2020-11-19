e9367fe72f190811b8e52ace7cb10427e6f956aa
William Fank Thomé <willegp88@gmail.com>
2020-11-19 11:41:03 -0300

    refactor: add newable array and factory array options to bind config

--- src/configs/bind.config.ts
+++ src/configs/bind.config.ts
@@ -27,10 +27,28 @@ export class BindConfig<I, P = I> extends BaseConfig<I, P> {
     return new ObjectConfig<I, P>(this.config)
   }
 
+  asNewableArray = (...array: Newable<P>[]): ArrayConfig<I, P> => {
+    this.config.oldValue = this.config.payload.value
+    const newArray = [...this.config.payload.newableArray || [], ...array]
+    this.config.payload.newableArray = newArray
+    this.config.newValue = null
+    this.config.args = array
+    return new ArrayConfig<I, P>(this.config)
+  }
+
   asFactory = (factory: Factory<P>): ObjectConfig<I, P> => {
     this.config.oldValue = this.config.payload.value
     this.config.payload.factory = factory
     this.config.newValue = null
     return new ObjectConfig<I, P>(this.config)
+  }
+
+  asFactoryArray = (...array: Factory<P>[]): ArrayConfig<I, P> => {
+    this.config.oldValue = this.config.payload.value
+    const newArray = [...this.config.payload.factoryArray || [], ...array]
+    this.config.payload.factoryArray = newArray
+    this.config.newValue = null
+    this.config.args = array
+    return new ArrayConfig<I, P>(this.config)
   }
 }
