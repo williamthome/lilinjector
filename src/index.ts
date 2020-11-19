625f12410141649546aba9ad2cc00d2683e4f3ed
William Fank Thomé <willegp88@gmail.com>
2020-11-19 15:55:32 -0300

    refactor: include injectable array to default export

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { Container } from './container'
 const container = new Container()
 const Inject = container.createInjectDecorator
 const Injectable = container.createInjectableDecorator
+const InjectableArray = container.createInjectableArrayDecorator
 
 export default container
-export { Inject, Injectable }
+export { Inject, Injectable, InjectableArray }
