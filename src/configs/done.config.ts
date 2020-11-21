84db2acd910aea1ec0cbdb1f1a46c958445168c2
William Fank Thomé <willegp88@gmail.com>
2020-11-21 07:50:16 -0300

    refactor: return container on done

--- src/configs/done.config.ts
+++ src/configs/done.config.ts
@@ -1,10 +1,12 @@
+import { Container } from 'src/container'
 import { BaseConfig } from './base.config'
 
 export class DoneConfig<I, P = I> extends BaseConfig<I, P> {
-  done = (): void => {
+  done = (): Container => {
     const { newValue, payload, container, identifier } = this.config
     if (newValue && !payload.noCache) payload.cache = newValue
     else if (newValue === null && payload.cache) delete payload.cache
     container.override(identifier, payload)
+    return container
   }
 }
